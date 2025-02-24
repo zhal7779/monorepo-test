7f2df838d66b83a10339ddc41cbd5be0b954f737
minyoung <zhal7779@naver.com>
2025-02-24 17:30:11 +0900

    chore: react-vue 서비스 환경설정

---
+++ apps/react-app/src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <div>
+      <h1>Hello World</h1>
+      <div>react-app</div>
+    </div>
+  </React.StrictMode>
+);
